65c50f8d19933552faa5ac751485c2391e64167e
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2021-08-08 18:20:54 +0200

    Initial Road to effective Snapshot Testing

---
+++ app/src/main/java/com/example/road/to/effective/snapshot/testing/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.road.to.effective.snapshot.testing
+
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.button.MaterialButton
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        window.decorView.rootView.findViewById<MaterialButton>(R.id.showDialogButton)
+            .setOnClickListener { openDialog() }
+
+    }
+
+    fun openDialog() {
+        val dialog = DialogBuilder.buildDeleteDialog(
+            this,
+            onDeleteClicked = {/* no-op*/ },
+            bulletTexts = arrayOf(
+                "Item one, undoubtedly the longest",
+                "Item two, also very long",
+                "Item three, short")
+        )
+        dialog.show()
+    }
+}
