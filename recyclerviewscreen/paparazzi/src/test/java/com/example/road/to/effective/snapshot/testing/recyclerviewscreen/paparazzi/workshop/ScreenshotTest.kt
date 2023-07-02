6ae67ff7e34249d6b4a4b58753e7ad04103307f8
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2023-07-02 19:49:57 +0200

    Add full command

--- recyclerviewscreen/paparazzi/src/test/java/com/example/road/to/effective/snapshot/testing/recyclerviewscreen/paparazzi/workshop/ScreenshotTest.kt
+++ recyclerviewscreen/paparazzi/src/test/java/com/example/road/to/effective/snapshot/testing/recyclerviewscreen/paparazzi/workshop/ScreenshotTest.kt
@@ -8,4 +8,6 @@ package com.example.road.to.effective.snapshot.testing.recyclerviewscreen.papara
  *
  * Execute tests in this package only by adding the following to the previous command
  * --tests '*ScreenshotTest'
+ *
+ * ./gradlew :recyclerviewscreen:paparazzi:recordPaparazziDebug --tests '*ScreenshotTest'
  */
