1356b7206d973b85be9f7be6c100855e4dca09d6
Sergio Sastre Flórez <sergio.sastre.florez@gmail.com>
2026-02-12 21:24:46 +0100

    add dynamic colors theme in lazycolumn

--- lazycolumnscreen/src/main/java/com/example/road/to/effective/snapshot/testing/lazycolumnscreen/Theme.kt
+++ lazycolumnscreen/src/main/java/com/example/road/to/effective/snapshot/testing/lazycolumnscreen/Theme.kt
@@ -1,11 +1,15 @@
 package com.example.road.to.effective.snapshot.testing.lazycolumnscreen
 
+import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.dynamicDarkColorScheme
+import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 
 val lightThemeColors = lightColorScheme(
     primary = Color(0xFF855446),
@@ -39,12 +43,15 @@ val darkThemeColors = darkColorScheme(
 @SuppressWarnings
 @Composable
 fun AppTheme(
+    darkTheme: Boolean = isSystemInDarkTheme(),
     content: @Composable () -> Unit
 ) {
-    val colors = if (isSystemInDarkTheme()) {
-        darkThemeColors
-    } else {
-        lightThemeColors
+    val dynamicColor = Build.VERSION.SDK_INT >= Build.VERSION_CODES.S
+    val colors = when {
+        dynamicColor && darkTheme -> dynamicDarkColorScheme(LocalContext.current)
+        dynamicColor && !darkTheme -> dynamicLightColorScheme(LocalContext.current)
+        darkTheme -> darkThemeColors
+        else -> lightThemeColors
     }
 
     MaterialTheme(
